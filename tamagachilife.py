bcdc5e0513178c200965afeaf32a1fe96d9f387a
SomeDudRice <Lucasl197@nycstudents.net>
2026-04-20 10:17:19 -0400

    Update tamagachilife.py

--- tamagachilife.py
+++ tamagachilife.py
@@ -9,22 +9,28 @@
 #         print(self.inventory)
 # john = Hero("john doe",20,{"title":"barsoap", "atk" : 999})
 # john.buy({"title": "Sword", "atk": 34})
-# print(john.__dict__)\
+# print(john.__dict__)
 
-class BankAccount:
-    def __init__(self, owner, balance):
-        self.owner = owner
-        self.__balance = balance  # double underscore means "private"
+# class BankAccount:
+#     def __init__(self, owner, balance):
+#         self.owner = owner
+#         self.__balance = balance  # double underscore means "private"
 
-    def deposit(self, amount):
-        self.__balance += amount
+#     def deposit(self, amount):
+#         self.__balance += amount
 
-    def show_balance(self):
-        print(f"{self.owner} has ${self.__balance}")
-John = BankAccount("john doe","9999999999$")
-print (John.__dict__)
-Jane = BankAccount("jane doe","0.000000000000000000-000-0-0000001$")
-print (Jane.__dict__)
-
+#     def show_balance(self):
+#         print(f"{self.owner} has ${self.__balance}")
+# John = BankAccount("john doe","9999999999$")
+# print (John.__dict__)
+ 
+class Pet:
+    def __init__(self, name, happiness, inventory):
+        self.name = name
+        self.__happiness = happiness
+        self.inventory = [inventory]
+    def play(self, happiness):
+    def showstatus(self, happiness):
+    
 
 
